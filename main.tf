1639a55cc080e489314383dca9e1b518d624eb1a
Lema Blanco <ylemablanco@deloitte.es>
2025-05-19 16:35:09 +0200

    esquema carpetas

---
+++ main.tf
@@ -0,0 +1,23 @@
+module "network" {
+  source = "./modules/network"
+}
+
+module "ec2" {
+  source     = "./modules/network"
+}
+
+module "s3" {
+  source = "./modules/s3"
+}
+
+module "iam" {
+  source = "./modules/iam"
+}
+
+module "cloudwatch" {
+  source = "./modules/cloudwatch"
+}
+
+module "budgets" {
+  source = "./modules/budgets"
+}
